929cc1e221e335283821980111b6239bb80955f3
Szymon Kapeniak <szymon.kapeniak@gmail.com>
2016-05-30 19:53:47 +0200

    Minimal setup for filter.

---
+++ src/VRAY_MagicMaskFilter.cpp
@@ -0,0 +1,136 @@
+/*
+ */
+
+#include <UT/UT_DSOVersion.h>
+
+#include "VRAY_MagicMaskFilter.hpp"
+#include <VRAY/VRAY_SpecialChannel.h>
+#include <UT/UT_Args.h>
+#include <UT/UT_StackBuffer.h>
+#include <SYS/SYS_Floor.h>
+#include <SYS/SYS_Math.h>
+
+using namespace HA_MMask;
+
+VRAY_PixelFilter *
+allocPixelFilter(const char *name)
+{
+    // NOTE: We could use name to distinguish between multiple pixel filters,
+    //       in the same library, but we only have one.
+    return new VRAY_MagicMaskFilter();
+}
+
+
+VRAY_MagicMaskFilter::VRAY_MagicMaskFilter()
+    : mySamplesPerPixelX(1) // Initialized just in case; value shouldn't be used
+    , mySamplesPerPixelY(1) // Initialized just in case; value shouldn't be used
+    , mySortByOpacity(false)
+    , mySortByPz(false)
+    , myUseOpID(false)
+    , myMaskNumber(4)
+{
+}
+
+VRAY_MagicMaskFilter::~VRAY_MagicMaskFilter()
+{
+}
+
+VRAY_PixelFilter *
+VRAY_MagicMaskFilter::clone() const
+{
+    // In this case, all of our members can be default-copy-constructed,
+    // so we don't need to write a copy constructor implementation.
+    VRAY_MagicMaskFilter *pf = new VRAY_MagicMaskFilter(*this);
+    return pf;
+}
+
+void
+VRAY_MagicMaskFilter::setArgs(int argc, const char *const argv[])
+{
+    UT_Args args;
+    args.initialize(argc, argv);
+    args.stripOptions("i:z:o:n:");
+
+    if (args.found('i')) {
+        myUseOpID = true;
+    }
+    if (args.found('z')) {
+        mySortByPz = true;
+    }
+    if (args.found('o')) {
+        mySortByOpacity = true;
+    }
+      if (args.found('n')) {
+       myMaskNumber =  args.iargp('n');
+    }      
+}
+
+void
+VRAY_MagicMaskFilter::getFilterWidth(float &x, float &y) const
+{
+    float filterwidth = 1.0f;
+    x = filterwidth;
+    y = filterwidth;
+}
+
+void
+VRAY_MagicMaskFilter::addNeededSpecialChannels(VRAY_Imager &imager)
+{
+    if (myUseOpID)
+        addSpecialChannel(imager, VRAY_SPECIAL_OPID);
+    if (mySortByPz)
+        addSpecialChannel(imager, VRAY_SPECIAL_PZ);
+
+    addSpecialChannel(imager, VRAY_SPECIAL_OPACITYSAMPLES);
+}
+
+
+
+void
+VRAY_MagicMaskFilter::prepFilter(int samplesperpixelx, int samplesperpixely)
+{
+    mySamplesPerPixelX = samplesperpixelx;
+    mySamplesPerPixelY = samplesperpixely;
+}
+
+void
+VRAY_MagicMaskFilter::filter(
+    float *destination,
+    int vectorsize,
+    const VRAY_SampleBuffer &source,
+    int channel,
+    int sourcewidth,
+    int sourceheight,
+    int destwidth,
+    int destheight,
+    int destxoffsetinsource,
+    int destyoffsetinsource,
+    const VRAY_Imager &imager) const
+{
+    const float *const opacitydata = \
+    getSampleData(source, getSpecialChannelIdx(imager, VRAY_SPECIAL_OPACITYSAMPLES));
+    const float *const zdata = mySortByPz
+        ? getSampleData(source, getSpecialChannelIdx(imager, VRAY_SPECIAL_PZ))
+        : NULL;
+    const float *const opiddata = myUseOpID
+        ? getSampleData(source, getSpecialChannelIdx(imager, VRAY_SPECIAL_OPID))
+        : NULL;
+
+    UT_ASSERT(opacitydata != NULL);
+    UT_ASSERT(mySortByPz == (zdata != NULL));
+    UT_ASSERT(myUseOpID == (opiddata != NULL));
+
+    // The gradient computations can be made much faster by separating x and y,
+    // using a temporary buffer, but this is implemented the slow way.
+    // In fact, nothing in here is optimized.
+
+    for (int desty = 0; desty < destheight; ++desty)
+    {
+        for (int destx = 0; destx < destwidth; ++destx)
+        {
+            float value = .12345;
+            for (int i = 0; i < vectorsize; ++i, ++destination)
+                *destination = value;
+        }
+    }
+}
---
+++ src/VRAY_MagicMaskFilter.hpp
@@ -0,0 +1,86 @@
+/*
+ Taken stright from HDK example.
+ */
+
+#pragma once
+
+#ifndef __VRAY_MagicMaskFilter__
+#define __VRAY_MagicMaskFilter__
+
+#include <VRAY/VRAY_PixelFilter.h>
+
+class VRAY_Imager;
+class VRAY_SampleBuffer;
+
+namespace HA_MMask {
+
+class VRAY_MagicMaskFilter : public VRAY_PixelFilter {
+public:
+    VRAY_MagicMaskFilter();
+    virtual ~VRAY_MagicMaskFilter();
+
+    virtual VRAY_PixelFilter *clone() const;
+
+    /// setArgs is called with the options specified after the pixel filter
+    /// name in the Pixel Filter parameter on the Mantra ROP.
+    /// -i 1 use OpId istead of 'mask' raster
+    /// -z 1 sort by Pz
+    /// -o 1 sort by opacity
+    /// -n 8 size of stored sampels per pixel. 
+ 
+    virtual void setArgs(int argc, const char *const argv[]);
+
+    /// getFilterWidth is called after setArgs when Mantra needs to know
+    /// how far to expand the render region.
+    virtual void getFilterWidth(float &x, float &y) const;
+
+    /// addNeededSpecialChannels is called after setArgs so that this filter
+    /// can indicate that it depends on having special channels like z-depths
+    /// or Op IDs.
+    virtual void addNeededSpecialChannels(VRAY_Imager &imager);
+
+    /// prepFilter is called after setArgs so that this filter can
+    /// precompute data structures or values for use in filtering that
+    /// depend on the number of samples per pixel in the x or y directions.
+    virtual void prepFilter(int samplesperpixelx, int samplesperpixely);
+
+    /// filter is called for each destination tile region with a source
+    /// that is at least as large as is needed by this filter, based on
+    /// the filter widths returned by getFilterWidth.
+    virtual void filter(
+        float *destination,
+        int vectorsize,
+        const VRAY_SampleBuffer &source,
+        int channel,
+        int sourcewidth,
+        int sourceheight,
+        int destwidth,
+        int destheight,
+        int destxoffsetinsource,
+        int destyoffsetinsource,
+        const VRAY_Imager &imager) const;
+
+private:
+    /// These must be saved in prepFilter.
+    /// Each pixel has mySamplesPerPixelX*mySamplesPerPixelY samples.
+    /// @{
+    int mySamplesPerPixelX;
+    int mySamplesPerPixelY;
+    /// @}
+
+    /// true if generate mask using Operator ID
+    bool myUseOpID;
+
+    /// true if sorting by Pz is enabled
+    bool mySortByPz;
+
+    /// true if sorting by Opacity is enabled
+    bool mySortByOpacity;
+
+    /// How many object we will store.
+    int myMaskNumber;
+};
+
+} // End HA_MMask namespace
+
+#endif
